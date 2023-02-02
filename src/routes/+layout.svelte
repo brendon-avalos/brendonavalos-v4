3961a3d90e6a0bb8558ab699e649035700fd1d5b
brendon-avalos <57079598+brendon-avalos@users.noreply.github.com>
2023-02-01 20:26:23 -0800

    using svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	import '../styles/all.css';
+</script>
+
+<header class="text-1">
+	<div class="name">Brendon Avalos</div>
+	<div>
+		<a href="/">Work</a>, <a href="">Info</a>, <a href="">Office Hours</a>
+	</div>
+</header>
+<main>
+	<slot />
+</main>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	header a {
+		text-decoration: none;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import '../styles/all.css';
+</script>
+
+<div class="container">
+	<div class="experience">Experience</div>
+</div>
+
+<div class="container">
+	<div class="current">Currently</div>
+	<div class="body-1 narrative">
+		I am a designer at Fictive Kin. I contribute to both web & digital product projects for
+		businesses large and small. I also manage projects and develop internal processes.
+	</div>
+</div>
+
+<style>
+	.experience {
+		justify-content: center;
+		align-items: center;
+		margin-top: calc(var(--spacing-unit) * 5);
+		margin-bottom: calc(var(--spacing-unit) * 5);
+		grid-column: col-start 4 / span 2;
+	}
+	.current {
+		grid-column: col-start 1 / span 2;
+	}
+
+	.narrative {
+		grid-column: col-start 7 / span 6;
+	}
+
+	@media (min-width: 1600px) {
+		.narrative {
+			grid-column: col-start 9 / span 4;
+		}
+	}
+</style>
