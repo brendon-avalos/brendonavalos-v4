ad60e0800f93b6953a21691355fee5c286eeee8d
brendon-avalos <57079598+brendon-avalos@users.noreply.github.com>
2023-02-11 01:04:51 -0800

    changing stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 <header>
 	<div class="name">
 		<div class="name_name">Brendon Avalos</div>
+		<div class="name_description">Designer based in Los Angeles.</div>
 	</div>
 	<div class="links">
 		<a href="/">Work</a>, <a href="">Info</a>, <a href="">Office Hours</a>
@@ -68,5 +69,15 @@
 		display: flex;
 		flex-direction: row;
 		gap: 1rem;
+	}
+
+	.name {
+		position: fixed;
+		bottom: 1rem;
+		left: 1.5rem;
+	}
+
+	.name_name {
+		text-indent: 2rem;
 	}
 </style>
